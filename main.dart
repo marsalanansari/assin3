38f4994564faca8472ef1bf33b1c0b04b5010f9d
marsalanansari <muhammadarsalanansari786@gmail.com>
2022-03-13 03:53:50 +0500

    assin complet

---
+++ main.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+void main() {
+  // ======== answer of question no 1 =========
+
+  // Logical Operators
+
+  //  && this is  (and) opretor
+  //  || this is  (or) opretor
+
+  //   Relational Operators
+
+  //   >  this is (Greater than) opretor
+  //   <  this is (Less than) opretor
+  //  ==  this is (Equal to) opretor
+  //  !=  this is (not Equal to) opretor
+
+  //  ======= answer of question no 2 =========
+
+  // var t_price = 600;
+  // var t_qunt = 5;
+  // var total = t_qunt * t_price;
+  // print('the ticket price is $t_price and you bill of $t_qunt ticket is $total');
+
+  //  ======= answer of question no 3 =========
+
+  // sawal hi samujh nhi aya  mujhe
+
+  //  ======= answer of question no 4 =========
+
+//  ??=  this will assinge value if variable in null
+//  and   this will return false if 1 value will false
+
+//  ======= answer of question no 5 =========
+
+//  number , string , boolean , integer , floot(desimail)
+// these all data type are suported in dart
+
+//  ======= answer of question no 6 =========
+
+  // var table = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
+  // var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var newnum = [];
+  // print(table.map((e) => num);
+
+  //  ======= answer of question no 7 =========
+
+  // var password = '249249';
+  // var u_pass = stdin.readLineSync();
+  // if(password == u_pass) {
+  //   print('Correct password');
+  // } else {
+  //   print('incorrect password');
+  // }
+
+  //  ======= answer of question no 8 =========
+}
